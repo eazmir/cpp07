bff78860cdd288625b59c99bcd26c44b948f2848
eazmir <s20092024@gmail.com>
2026-02-11 12:41:16 +0100

    done

---
+++ ex00/include/whatever.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+template <typename T>
+
+void swap(T &a ,T &b)
+{
+    T tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+template <typename T>
+
+T min(T a , T b)
+{
+    T cmp = (a > b) ? b : a;
+    return (cmp);
+}
+template <typename T>
+
+T max(T a , T b)
+{
+    T cmp = (a > b) ? a : b;
+    return (cmp);
+}
